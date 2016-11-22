c4da937e6663fd4e2f14f5e22417c6593ab6392b
Jack Draak <jackdraak@gmail.com>
2016-11-21 23:55:21 -0500

    need to chose between two 'IsIsogramMatched' options in the code... mostly tidy

--- IsogramGame/IsogramGame.cpp
+++ IsogramGame/IsogramGame.cpp
@@ -3,34 +3,49 @@
 
 using int32 = int;
 
-void IsogramGame::IncrementGuess()              { iCurrentGuess++; }
-void IsogramGame::EvaluateGuess() const         { return; } // TODO code this
-int32 IsogramGame::iGetMaxGuesses() const       { return iMaxGuesses; }
+bool IsogramGame::bIsIsogramRevealed() const    { return bIsIsogramMatched; } 
 int32 IsogramGame::iGetCurrentGuess() const     { return iCurrentGuess; }
 int32 IsogramGame::iGetIsogramLength() const    { return iIsogramLength; }
-bool IsogramGame::bIsIsogramRevealed() const    { return false; } // TODO code this
-
-Analysis IsogramGame::AnalyzeGuess(FString sGuess)
-{
-    // setup return variable
-    Analysis analysis;
-
-    for (int32 iGuessLetter = 0; iGuessLetter < iIsogramLength; iGuessLetter++) {
-        for (int32 iIsogramLetter = 0; iIsogramLetter < iIsogramLength; iIsogramLetter++) {
-            if (sGuess[iGuessLetter] == sIsogram[iIsogramLetter] && iGuessLetter == iIsogramLetter) { // could be cleaner on more lines
-                analysis.iPositionMatches++;
-            } else if (iGuessLetter != iIsogramLetter && sGuess[iGuessLetter] == sIsogram[iIsogramLetter]) {
-                analysis.iLetterMatches++;
-            }
-        }
-    }
-    return analysis;
-}
+int32 IsogramGame::iGetMaxGuesses() const       { return iMaxGuesses; }
+void IsogramGame::IncrementGuess()              { iCurrentGuess++; }
 
 IsogramGame::IsogramGame()
 {
     iMaxGuesses = 4;
     Reset();
+}
+
+Analysis IsogramGame::AnalyzeGuess(FString sGuess)
+{
+    Analysis analysis; // setup return variable
+
+    for (int32 iGuessLetter = 0; iGuessLetter < iIsogramLength; iGuessLetter++) {
+        for (int32 iIsogramLetter = 0; iIsogramLetter < iIsogramLength; iIsogramLetter++) {
+            if (sGuess[iGuessLetter] == sIsogram[iIsogramLetter]) {
+                if (iGuessLetter == iIsogramLetter) 
+                {
+                    analysis.iPositionMatches++; // TODO score
+                }
+                else // implicit: if (iGuessLetter != iIsogramLetter) {
+                { 
+                    analysis.iLetterMatches++; // TODO score
+                }
+            }
+        }
+    }
+
+    if (analysis.iPositionMatches == iIsogramLength) 
+    {
+        // TODO need to settle on one of these rather than doing both....
+        bIsIsogramMatched = true;
+        analysis.bDoesGuessMatchIsogram = true;
+    }
+    else {
+        // TODO need to settle on one of these rather than doing both....
+        bIsIsogramMatched = false;
+        analysis.bDoesGuessMatchIsogram = false;
+    }
+    return analysis;
 }
 
 std::string IsogramGame::SelectIsogram()
@@ -43,10 +58,11 @@ void IsogramGame::Reset()
     if (!bInitialized) 
     {
         bInitialized = true;
+        iLossCount = 0;
         iScore = 0;
         iWinCount = 0;
-        iLossCount = 0;
     }
+    bIsIsogramMatched = false;
     iCurrentGuess = 1;
     sIsogram = SelectIsogram();
     iIsogramLength = sIsogram.length();
--- IsogramGame/IsogramGame.h
+++ IsogramGame/IsogramGame.h
@@ -16,24 +16,24 @@ class IsogramGame {
 public:
     IsogramGame(); // constructor 
 
+    Analysis AnalyzeGuess(FString); // requires validated input
     bool bIsIsogramRevealed() const;
-    void Reset();
-    void EvaluateGuess() const;
-    void IncrementGuess();
+    FString SelectIsogram();
     int32 iGetCurrentGuess() const;
     int32 iGetIsogramLength() const;
     int32 iGetMaxGuesses() const;
-    Analysis AnalyzeGuess(FString); // requires validated input
-    FString SelectIsogram();
+    void IncrementGuess();
+    void Reset();
 
     // see IsogramGame::IsogramGame() definition for initialization
 private:
+    bool bInitialized;
+    bool bIsIsogramMatched;
+    FString sIsogram;
     int32 iCurrentGuess;
-    int32 iMaxGuesses;
     int32 iIsogramLength;
+    int32 iLossCount;
+    int32 iMaxGuesses;
     int32 iScore;
     int32 iWinCount;
-    int32 iLossCount;
-    bool bInitialized;
-    FString sIsogram;
 };
